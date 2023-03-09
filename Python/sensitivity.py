edf302b99e3ec1e66198a03b0479fdf16e9ed244
Agathe By Raaum <agathebyraaum@Agathes-MacBook-Pro.local>
2023-03-09 17:54:39 +0100

    m

--- Python/sensitivity.py
+++ Python/sensitivity.py
@@ -209,7 +209,7 @@ if __name__ == "__main__":
 
     for i in range(len(type_hf)):
         for j in range(len(params)):
-            V, Cai, Ta, CaTrpn, lam = isometric_sensitivity(
+            V, Cai, Ta, CaTrpn = isometric_sensitivity(
                 hf_type=type_hf[i], 
                 cell_type='endo', 
                 mech_param=params[j], 
