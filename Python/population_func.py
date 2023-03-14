05d6b9481900813155734d5f0cd8e1e45a383e01
Agathe By Raaum <agathebyraaum@Agathes-MacBook-Pro.local>
2023-03-14 15:56:24 +0100

    started implementation of population model

---
+++ Python/population_func.py
@@ -0,0 +1,157 @@
+"""
+Script for extracting different biomarkers for cleaning populaiton model.
+
+TODO:
+1. Fix repeating code, put in one func
+"""
+import ORdmm_Land_em_coupling as model
+import numpy as np
+
+def apd_values(y, percentage=0.9, index=False):
+    # extract membrane potential
+    V = y.T[model.state_indices('v')]
+    V_max_idx = np.argmax(V)
+    V_tmp = V[V_max_idx:]
+
+    # find value of range [min V, max V] at % repolarization
+    V_adp = (1-percentage)*(max(V)-min(V))+min(V)
+
+    # find the index
+    apd_idx_tmp = (np.abs(V_tmp - V_adp)).argmin() 
+    apd_idx = V_max_idx + apd_idx_tmp
+    if index == True:
+        return apd_idx 
+    else:
+        #time in ms
+        return apd_idx/10
+
+
+def catd_values(y, percentage=0.8, index=False):
+    # extract Ca_i
+    Cai = y.T[model.state_indices('cai')]
+    Cai_max_idx = np.argmax(Cai)
+    Cai_tmp = Cai[Cai_max_idx:]
+
+    # find value of range [min Cai, max Cai] at % repolarization
+    CaTD = (1-percentage)*(max(Cai)-min(Cai))+min(Cai)
+
+    # find the index
+    CaTD_idx_tmp = (np.abs(Cai_tmp - CaTD)).argmin() 
+    CaTD_idx = Cai_max_idx + CaTD_idx_tmp
+    if index == True:
+        return CaTD_idx 
+    else:
+        #time in ms
+        return CaTD_idx/10
+
+
+# monitored
+def tad_values(monitor, percentage=0.5, index=False):
+    # extract Ta
+    Ta = monitor.T[model.monitor_indices("Ta")]
+    Ta_max_idx = np.argmax(Ta)
+    Ta_tmp = Ta[Ta_max_idx:]
+
+    # find value of range [min Ta, max Ta] at % repolarization
+    TaD = (1-percentage)*(max(Ta)-min(Ta))+min(Ta)
+
+    # find the index
+    TaD_idx_tmp = (np.abs(Ta_tmp - TaD)).argmin() 
+    TaD_idx = Ta_max_idx + TaD_idx_tmp
+    if index == True:
+        return TaD_idx 
+    else:
+        #time in ms
+        return TaD_idx/10
+
+
+def state_biomarkers(y):
+    """Simplified ap/cai max/min values for population.
+    """
+    V = y.T[model.state_indices('v')]
+    Cai = y.T[model.state_indices('cai')]
+
+    return min(V), max(V), min(Cai), max(Cai)
+
+
+def monitored_biomarkers(monitor):
+    """Simplified dv_dt/Ta values for population.
+    """
+    Ta = monitor.T[model.monitor_indices("Ta")]
+    Ta_max_idx = np.argmax(Ta)/10 # ms
+
+    dvdt = monitor.T[model.monitor_indices("dv_dt")]
+
+    return Ta_max_idx, min(Ta), max(Ta), max(dvdt)
+
+
+
+#################################
+# Specific (repetative) functions
+#################################
+
+def ap_max(y, time=False):
+    V = y.T[model.state_indices('v')]
+    if time == True:
+        V_max_idx = np.argmax(V)
+        return V_max_idx/10 #time in ms
+    else:
+        return max(V)
+
+
+def ap_min(y, time=False):
+    V = y.T[model.state_indices('v')]
+    if time == True:
+        V_min_idx = np.argmin(V)
+        return V_min_idx/10 #time in ms
+    else:
+        return min(V)
+
+
+def max_cai(y, time=False):
+    # systolic
+    Cai = y.T[model.state_indices('cai')]
+    if time == True:
+        Cai_max_idx = np.argmax(Cai)
+        return Cai_max_idx/10 #time in ms
+    else:
+        return max(Cai)
+
+
+def min_cai(y, time=False):
+    # diastolic
+    Cai = y.T[model.state_indices('cai')]
+    if time == True:
+        Cai_min_idx = np.argmin(Cai)
+        return Cai_min_idx/10 #time in ms
+    else:
+        return min(Cai)
+
+#monitored
+def ta_max(monitor, time=False):
+    Ta = monitor.T[model.monitor_indices("Ta")]
+    if time == True:
+        Ta_max_idx = np.argmax(Ta)
+        return Ta_max_idx/10 #time in ms
+    else:
+        return max(Ta)
+
+# monitored
+def ta_min(monitor, time=False):
+    Ta = monitor.T[model.monitor_indices("Ta")]
+    if time == True:
+        Ta_min_idx = np.argmin(Ta)
+        return Ta_min_idx/10 #time in ms
+    else:
+        return min(Ta)
+
+# monitored
+def dvdt_max(monitor, time=False):
+    dvdt = monitor.T[model.monitor_indices("dv_dt")]
+    if time == True:
+        dvdt_max_idx = np.argmax(dvdt)
+        return dvdt_max_idx/10 #time in ms
+    else:
+        return max(dvdt)
+
+
---
+++ Python/population_model.py
@@ -0,0 +1,273 @@
+import ORdmm_Land_em_coupling as model
+from scipy.integrate import odeint
+import numpy as np
+import os
+import matplotlib.pyplot as plt
+import tqdm
+import pandas as pd
+import time
+
+from population_func import apd_values, catd_values, tad_values, state_biomarkers, monitored_biomarkers #ap_max, dvdt_max
+
+num_beats = 10
+tsteps = np.arange(0.0, 1000.0, 0.1)  # real run 1000
+pop_size = 10
+# lambda = 1
+
+def random_sampling(pop_size=pop_size, hf_type='control', mech_type='iso'):
+    """Random sampling (pop_size x 12 parameters) from normal distribution"""
+    np.random.seed(10)
+    all_val = np.random.normal(loc=1, scale = 0.15, size=10000)
+    selected_val = np.random.choice(all_val, (pop_size,12), replace=True)
+    # save to file
+    np.save(f"rand_sample_{mech_type}_{hf_type}.npy", selected_val)
+
+
+def make_population(hf_type, cell_type, mech_type, out=None):
+    """Make population of models.
+    """
+    # load random sampling values
+    rand_val = np.load(f'rand_sample_{mech_type}_{hf_type}.npy') 
+    population = []
+
+    for i in range(10):
+        y0 = np.load(
+            f"init_values/coupled/{hf_type}_{cell_type}_coupled_{mech_type}_100.npy"
+        )
+        parameters = model.init_parameter_values(
+            celltype=0 if cell_type == "endo" else 1 if cell_type == "epi" else 2,
+            isometric=1 if mech_type=='iso' else 0,
+            lmbda_set=1,
+            #mechanical parameters
+            ku_rate=rand_val[i][0],
+            kuw_rate=rand_val[i][1],
+            kws_rate=rand_val[i][2],
+            ktrpn_rate=rand_val[i][3],
+            Trpn50_rate=rand_val[i][4],
+            gammaw_rate=rand_val[i][5],
+            gammas_rate=rand_val[i][6],
+            rs_rate=rand_val[i][7],
+            rw_rate=rand_val[i][8],
+            Tref_rate=rand_val[i][9],
+            cat50ref_rate=rand_val[i][10],
+            ntm_rate=rand_val[i][11],
+            #HF parameters
+            GNaL_rate=1.80 if hf_type=='gomez' else 1,
+            Gto_rate=0.40 if hf_type=='gomez' else 1,
+            GK1_rate=0.68 if hf_type=='gomez' else 1,
+            Gncx_rate=1.750 if hf_type=='gomez' else 1,
+            Jleak_rate=1.30 if hf_type=='gomez' else 1,
+            Jserca_rate=0.5 if hf_type=='gomez' else 1,
+            CaMKa_rate=1.50 if hf_type=='gomez' else 1,
+            Pnak_rate=0.70 if hf_type=='gomez' else 1,
+            Pnab_rate=1,
+            Pcab_rate=1,
+            thl_rate=1.80 if hf_type=='gomez' else 1,
+            Jrel_inf_sensitivity=0.80 if hf_type=='gomez' else 1,
+            Jrel_infp_sensitivity=0.80 if hf_type=='gomez' else 1,
+        )
+        
+        for n in tqdm.tqdm(range(num_beats)):
+            y = odeint(model.rhs, y0, tsteps, args=(parameters,))
+            y0 = y[-1]
+
+        population.append(y0)
+    
+    np.save(f'population_{mech_type}_{hf_type}.npy', population, allow_pickle=True)
+    
+
+def plot_population(hf_type='control', cell_type='endo', mech_type='iso'):
+    """Load population -> clean data"""
+    # load random sampling values
+    rand_val = np.load(f'rand_sample_{mech_type}_{hf_type}.npy') 
+    # load population
+    y0s = np.load(f'population_{mech_type}_{hf_type}.npy')
+
+    fig, ax = plt.subplots(2, 2, sharex=True, figsize=(14,8))
+
+    for i in range(pop_size):
+        y0 = y0s[i]
+
+        parameters = model.init_parameter_values(
+            celltype=0 if cell_type == "endo" else 1 if cell_type == "epi" else 2,
+            isometric=1 if mech_type=='iso' else 0,
+            lmbda_set=1,
+            #mechanical parameters
+            ku_rate=rand_val[i][0],
+            kuw_rate=rand_val[i][1],
+            kws_rate=rand_val[i][2],
+            ktrpn_rate=rand_val[i][3],
+            Trpn50_rate=rand_val[i][4],
+            gammaw_rate=rand_val[i][5],
+            gammas_rate=rand_val[i][6],
+            rs_rate=rand_val[i][7],
+            rw_rate=rand_val[i][8],
+            Tref_rate=rand_val[i][9],
+            cat50ref_rate=rand_val[i][10],
+            ntm_rate=rand_val[i][11],
+            #HF parameters
+            GNaL_rate=1.80 if hf_type=='gomez' else 1,
+            Gto_rate=0.40 if hf_type=='gomez' else 1,
+            GK1_rate=0.68 if hf_type=='gomez' else 1,
+            Gncx_rate=1.750 if hf_type=='gomez' else 1,
+            Jleak_rate=1.30 if hf_type=='gomez' else 1,
+            Jserca_rate=0.5 if hf_type=='gomez' else 1,
+            CaMKa_rate=1.50 if hf_type=='gomez' else 1,
+            Pnak_rate=0.70 if hf_type=='gomez' else 1,
+            Pnab_rate=1,
+            Pcab_rate=1,
+            thl_rate=1.80 if hf_type=='gomez' else 1,
+            Jrel_inf_sensitivity=0.80 if hf_type=='gomez' else 1,
+            Jrel_infp_sensitivity=0.80 if hf_type=='gomez' else 1,
+        )
+
+        y = odeint(model.rhs, y0, tsteps, args=(parameters,))
+        monitor = np.array(
+                [model.monitor(r, t, parameters) for r, t in zip(y, tsteps)]
+            )
+        
+        V = y.T[model.state_indices("v")]
+        Cai = y.T[model.state_indices("cai")]
+        Ta = monitor.T[model.monitor_indices("Ta")]
+        CaTrpn = y.T[model.state_indices("CaTrpn")]
+        
+        ax[0][0].plot(tsteps, V, linewidth=0.7)
+        ax[0][0].set_title("Voltage")
+        ax[0][0].set_ylabel("Voltage (mV)")
+        ax[0][0].set_xlabel("Time (ms)")
+        ax[0][0].grid(linewidth=0.3)
+
+        ax[0][1].plot(tsteps, Cai, linewidth=0.7)
+        ax[0][1].set_title("Cai")
+        ax[0][1].set_ylabel("Ca_i (mM)")
+        ax[0][1].set_xlabel("Time (ms)")
+        ax[0][1].grid(linewidth=0.3)
+
+        ax[1][0].plot(tsteps, Ta, linewidth=0.7)
+        ax[1][0].set_title("Ta")
+        ax[1][0].set_ylabel("Ta ()")
+        ax[1][0].set_xlabel("Time (ms)")
+        ax[1][0].grid(linewidth=0.3)
+
+        ax[1][1].plot(tsteps, CaTrpn, linewidth=0.7)
+        ax[1][1].set_title("CaTrpn")
+        ax[1][1].set_ylabel("CaTrpn ()")
+        ax[1][1].set_xlabel("Time (ms)")
+        ax[1][1].grid(linewidth=0.3)
+
+    plt.show()
+
+
+def clean_population(hf_type='control', cell_type='endo', mech_type='iso'):
+    """NOT FINISHED, missing mechanical check and save
+    Load population -> clean data -> new population"""
+    # load random sampling values
+    rand_val = np.load(f'rand_sample_{mech_type}_{hf_type}.npy') 
+    # load population
+    y0s = np.load(f'population_{mech_type}_{hf_type}.npy')
+
+    remove_pop = []
+
+    for i in range(pop_size):
+        y0 = y0s[i]
+
+        parameters = model.init_parameter_values(
+            celltype=0 if cell_type == "endo" else 1 if cell_type == "epi" else 2,
+            isometric=1 if mech_type=='iso' else 0,
+            lmbda_set=1,
+            #mechanical parameters
+            ku_rate=rand_val[i][0],
+            kuw_rate=rand_val[i][1],
+            kws_rate=rand_val[i][2],
+            ktrpn_rate=rand_val[i][3],
+            Trpn50_rate=rand_val[i][4],
+            gammaw_rate=rand_val[i][5],
+            gammas_rate=rand_val[i][6],
+            rs_rate=rand_val[i][7],
+            rw_rate=rand_val[i][8],
+            Tref_rate=rand_val[i][9],
+            cat50ref_rate=rand_val[i][10],
+            ntm_rate=rand_val[i][11],
+            #HF parameters
+            GNaL_rate=1.80 if hf_type=='gomez' else 1,
+            Gto_rate=0.40 if hf_type=='gomez' else 1,
+            GK1_rate=0.68 if hf_type=='gomez' else 1,
+            Gncx_rate=1.750 if hf_type=='gomez' else 1,
+            Jleak_rate=1.30 if hf_type=='gomez' else 1,
+            Jserca_rate=0.5 if hf_type=='gomez' else 1,
+            CaMKa_rate=1.50 if hf_type=='gomez' else 1,
+            Pnak_rate=0.70 if hf_type=='gomez' else 1,
+            Pnab_rate=1,
+            Pcab_rate=1,
+            thl_rate=1.80 if hf_type=='gomez' else 1,
+            Jrel_inf_sensitivity=0.80 if hf_type=='gomez' else 1,
+            Jrel_infp_sensitivity=0.80 if hf_type=='gomez' else 1,
+        )
+
+        y = odeint(model.rhs, y0, tsteps, args=(parameters,))
+        monitor = np.array(
+                [model.monitor(r, t, parameters) for r, t in zip(y, tsteps)]
+            )
+        
+        # biomarkers from Mora
+        APD_40 = apd_values(y=y, percentage=0.4)
+        APD_50 = apd_values(y=y, percentage=0.5)
+        APD_90 = apd_values(y=y, percentage=0.9)
+        Tri90_40 = APD_90 - APD_40
+        CaTD_50 = catd_values(y=y, percentage=0.5)
+        CaTD_90 = catd_values(y=y, percentage=0.9)
+        RT50 = tad_values(monitor=monitor, percentage=0.5, index=False)
+        RT95 = tad_values(monitor=monitor, percentage=0.95, index=False) #Says both 90 and 95 in paper
+        # eq's beneath can be split into different calls if uncertain, see population_func
+        TTP, Ta_min, Ta_max, dvdt_max = monitored_biomarkers(monitor=monitor)
+        RMP, V_peak, diast_Ca, syst_Ca = state_biomarkers(y=y)
+
+        # cost function for mechanical biomarkers
+
+        d_TTP = 0 if TTP >= 147 and TTP <= 172 else min(abs(TTP-147), abs(TTP-172)) # 147, 172
+        d_RT50 = 0 if RT50 >= 109 and RT50 <= 125 else min(abs(RT50-109), abs(RT50-125)) # 109, 125
+        d_RT95 = 0 if RT95 >= 291 and RT95 <= 377 else min(abs(RT95-291), abs(RT95-377)) # 291, 377
+        d_Ta_max = 0 if Ta_max >= 15 and Ta_max <= 25 else min(abs(Ta_max-15), abs(Ta_max-25)) # 15, 25
+
+        d_t = d_TTP + d_RT50 + d_RT95 + 10*d_Ta_max + 25*Ta_min
+
+        # check electrophysiological biomarkers
+        print(f'Cell number {i}')
+        print(d_t)
+        if APD_40 >= 85 and APD_40 <= 320 and \
+            APD_50 >= 110 and APD_50 <= 350 and \
+            APD_90 >= 180 and APD_90 <= 440 and \
+            Tri90_40 >= 50 and Tri90_40 <= 150 and \
+            dvdt_max >= 150 and dvdt_max <= 1000 and \
+            V_peak >= 10 and V_peak <= 55 and \
+            RMP >= -95 and RMP <= -80 and \
+            CaTD_50 >= 120 and CaTD_50 <= 420 and \
+            CaTD_90 >= 220 and CaTD_90 <= 785 and \
+            syst_Ca >= 0.000262 and syst_Ca <= 0.00223 and \
+            diast_Ca >= 0 and diast_Ca <= 0.00040 and \
+            d_t < 5:
+
+            pass 
+
+        else:
+            remove_pop.append(i)
+        
+        # check mechanical biomarkers
+    
+    new_parameters = rand_val.copy() #for safety
+    new_population = y0s.copy() #for safety
+
+    new_parameters = np.delete(new_parameters, remove_pop, 0)
+    new_population = np.delete(new_population, remove_pop, 0)
+    print(remove_pop)
+    
+
+        
+           
+
+
+if __name__ == "__main__":
+    #random_sampling(hf_type='control', mech_type='iso')
+    #make_population(hf_type='control', cell_type='endo', mech_type='iso')
+    #plot_population(hf_type='control', cell_type='endo', mech_type='iso')
+    clean_population()
