e5a30d4d94c6e34c794739192ab422189ca5bb02
Agathe By Raaum <agathebyraaum@Agathes-MacBook-Pro.local>
2023-03-30 13:08:36 +0200

    time test

--- Python/drug_plot.py
+++ Python/drug_plot.py
@@ -13,24 +13,23 @@ import itertools
 from drug_values import drug_dict
 
 tsteps = np.arange(0.0, 1000.0, 0.1)  # real run 1000
-pop_size = 1000
+pop_size = 10
 
-def get_pop_drug_val(mech_type, hf_type, part, drug_type, cell_type='endo'):
+def plot_pop_drug(mech_type, hf_type, drug_type, cell_type='endo'):
     """Run the population model with different drugs.
     """
     # load random sampling values
     rand_val = np.load(f'init_pop/rand_sample_iso_control.npy')
     # load specific population
     y0s = np.load(f'init_pop_drug/population_{mech_type}_{hf_type}_{drug_type}.npy') 
-
-    part_dict = {
-        '1': [0,200], '2': [200,400], '3': [400,600], '4': [600,800], '5': [800,1000], 
-        #'6': [500,600], '7': [600,700], '8': [700,800], '9': [800,900], '10': [900,1000]
-        }
     
     Vs, Cais, Tas, CaTrpns = [], [], [], []
 
-    for i in range(part_dict[part][0], part_dict[part][1]):
+    if mech_type == 'dyn':
+        Lambdas = []
+
+    fig, ax = plt.subplots(2, 2, sharex=True, figsize=(14,8))
+    for i in range(pop_size):
         print(i)
         y0 = y0s[i]
 
@@ -101,77 +100,63 @@ def get_pop_drug_val(mech_type, hf_type, part, drug_type, cell_type='endo'):
         Cais.append(Cai)
         Tas.append(Ta)
         CaTrpns.append(CaTrpn)
+
+        if mech_type =='dyn':
+            Lambda = y.T[model.state_indices("lmbda")]
+            Lambdas.append(Lambda)
+        
+        ax[0][0].plot(tsteps, V, linewidth=0.7, alpha=0.5, color='lightskyblue')
+        ax[0][1].plot(tsteps, Cai, linewidth=0.7, alpha=0.5, color='lightskyblue')
+        ax[1][0].plot(tsteps, Ta, linewidth=0.7, alpha=0.5, color='lightskyblue')
+        ax[1][1].plot(tsteps, CaTrpn if mech_type=='iso' else Lambda, linewidth=0.7, alpha=0.5, color='lightskyblue')
+
+    ax[0][0].plot(tsteps, np.mean(Vs, axis=0), linewidth=1, alpha=1, color='tab:blue')
+    ax[0][0].set_title("Voltage")
+    ax[0][0].set_ylabel("Voltage (mV)")
+    ax[0][0].set_xlabel("Time (ms)")
+    ax[0][0].grid(linewidth=0.3)
+
+    ax[0][1].plot(tsteps, np.mean(Cais, axis=0), linewidth=1, alpha=1, color='tab:blue')
+    ax[0][1].set_title("Cai")
+    ax[0][1].set_ylabel("Ca_i (mM)")
+    ax[0][1].set_xlabel("Time (ms)")
+    ax[0][1].grid(linewidth=0.3)
+
+    ax[1][0].plot(tsteps, np.mean(Tas, axis=0), linewidth=1, alpha=1, color='tab:blue')
+    ax[1][0].set_title("Ta")
+    ax[1][0].set_ylabel("Ta (kPa)")
+    ax[1][0].set_xlabel("Time (ms)")
+    ax[1][0].grid(linewidth=0.3)
+
+    ax[1][1].plot(tsteps, np.mean(CaTrpns, axis=0) if mech_type=='iso' else np.mean(Lambdas, axis=0), linewidth=1, alpha=1, color='tab:blue')
+    ax[1][1].set_title("CaTrpn" if mech_type=='iso' else "Lambda")
+    ax[1][1].set_ylabel("CaTrpn" if mech_type=='iso' else "Lambda")
+    ax[1][1].set_xlabel("Time (ms)")
+    ax[1][1].grid(linewidth=0.3)
+
+    fig.suptitle(f'{drug_type} ({mech_type}, {hf_type})')
+    leg = plt.figlegend(labels=['population', 'mean'], loc=7)
+    leg.legendHandles[0].set_color('lightskyblue')
+    leg.legendHandles[1].set_color('tab:blue')
     
-    full_lst = [Vs, Cais, Tas, CaTrpns]
-    
-    np.save(f'val_{mech_type}_{hf_type}_{drug_type}_{part}.npy', full_lst, allow_pickle=True) 
-
-            
-def plot_pop_drug(mech_type, hf_type, drug_type):
-
-    Vs, Cais, Tas, CaTrpns = [], [], [], []
-
-    for i in range(1,6):
-        arr = np.load(f'val_{mech_type}_{hf_type}_{drug_type}_{i}.npy')
-        Vs.append(arr[0])
-        Cais.append(arr[1])
-        Tas.append(arr[2])
-        CaTrpns.append(arr[3])
-        # delete the file after
-        # os.remove(f'val_{mech_type}_{hf_type}_{drug_type}_{i}.npy')
+    plt.savefig(f'plots/pop_drug_{hf_type}_{mech_type}_{drug_type}.png')
 
 
-    
-    V = list(itertools.chain(*Vs))
-    Cai = list(itertools.chain(*Cais))
-    Ta = list(itertools.chain(*Tas))
-    CaTrpn = list(itertools.chain(*CaTrpns))
-
-    fig, ax = plt.subplots(2, 2, sharex=True, figsize=(14,8))
-
-    for i in range(1000):
-        ax[0][0].plot(tsteps, V[i], linewidth=0.7, alpha=0.5, color='lightskyblue')
-        ax[0][0].set_title("Voltage")
-        ax[0][0].set_ylabel("Voltage (mV)")
-        ax[0][0].set_xlabel("Time (ms)")
-        ax[0][0].grid(linewidth=0.3)
-
-        ax[0][1].plot(tsteps, Cai[i], linewidth=0.7, alpha=0.5, color='lightskyblue')
-        ax[0][1].set_title("Cai")
-        ax[0][1].set_ylabel("Ca_i (mM)")
-        ax[0][1].set_xlabel("Time (ms)")
-        ax[0][1].grid(linewidth=0.3)
-
-        ax[1][0].plot(tsteps, Ta[i], linewidth=0.7, alpha=0.5, color='lightskyblue')
-        ax[1][0].set_title("Ta")
-        ax[1][0].set_ylabel("Ta (kPa)")
-        ax[1][0].set_xlabel("Time (ms)")
-        ax[1][0].grid(linewidth=0.3)
-
-        ax[1][1].plot(tsteps, CaTrpn[i], linewidth=0.7, alpha=0.5, color='lightskyblue')
-        ax[1][1].set_title("CaTrpn")
-        ax[1][1].set_ylabel("CaTrpn")
-        ax[1][1].set_xlabel("Time (ms)")
-        ax[1][1].grid(linewidth=0.3)
-    
-    plt.show()
 
     
-    
-
 
 if __name__ == '__main__':
-    """ 
+    drug = sys.argv[1]
+    mech = ['iso', 'dyn']
+    hf = ['control', 'gomez']
+    start = time.time()
     proc = []
-    for i in range(1,6):
-        p = Process(
-            target=get_pop_drug_val, 
-            args=('iso', 'control', str(i), 'verapamil')
-            )
-        p.start()
-        proc.append(p)
+    for m in mech:
+        for h in hf:
+            p = Process(target=plot_pop_drug, args=(m, h, drug))
+            p.start()
+            proc.append(p)
     for p in proc:
         p.join()
-    """
-    
-    plot_pop_drug('iso', 'control', 'verapamil')
+    end = time.time()
+    print(end-start)
